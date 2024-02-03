e5db03045591802a681da3463dbdfaccb09bac4b
saroj adhikari <adhikarisaroj291@gmail.com>
2024-02-03 18:55:43 +0545

    expense tracker complete

--- Expense-tracker/expenses-tracker/src/Context/GlobalState.js
+++ Expense-tracker/expenses-tracker/src/Context/GlobalState.js
@@ -15,15 +15,33 @@ const initialState = {
 export const GlobalContext = createContext(initialState);
 
 //provider component
-export const GlobalProvider = ({ childeren }) => {
+export const GlobalProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AppReducer, initialState);
+
+  // Actions
+  function deleteTransaction(id) {
+    dispatch({
+      type: "DELETE_TRANSACTION",
+      payload: id,
+    });
+  }
+
+  function AddTransaction(transaction) {
+    dispatch({
+      type: "ADD_TRANSACTION",
+      payload: transaction,
+    });
+  }
+
   return (
     <GlobalContext.Provider
       value={{
         transactions: state.transactions,
+        deleteTransaction,
+        AddTransaction,
       }}
     >
-      {childeren}
+      {children}
     </GlobalContext.Provider>
   );
 };
